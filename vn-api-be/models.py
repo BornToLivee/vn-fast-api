ccb589867d1d5863ff991d73cab2f077093abb3d
BornToLivee <bogdan.zinchenko.2019@gmail.com>
2025-03-21 13:44:33 +0200

    define simple models

---
+++ vn-api-be/models.py
@@ -0,0 +1,38 @@
+from sqlalchemy import Column, Integer, String, Table, ForeignKey
+from sqlalchemy.orm import relationship, declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+Base = declarative_base()
+
+
+novel_tag = Table(
+    "novel_tag", Base.metadata,
+    Column("user_id", Integer, ForeignKey("novels.id")),
+    Column("tags_id", Integer, ForeignKey("tags.id"))
+)
+
+
+class Novel(Base):
+    __tablename__ = "novels"
+    id = Column(Integer, primary_key=True)
+    title = Column(String, unique=True)
+    description = Column(String, required=False ,unique=False)
+    my_rating = Column(Integer, unique=False)
+    tags = relationship("Tag", secondary="novel_tag", back_populates="novels")
+
+
+class Tag(Base):
+    __tablename__ = "tags"
+    id = Column(Integer, primary_key=True)
+    name = Column(String, unique=True)
+    description = Column(String, required=False ,unique=False)
+    novels = relationship("Novel", secondary="novel_tag", back_populates="tags")
+
+
+DATABASE_URL = "sqlite:///./testnovels.db"
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine)
+
+Base.metadata.create_all(bind=engine)
