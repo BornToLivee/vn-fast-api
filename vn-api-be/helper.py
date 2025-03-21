faf7c98f28a8ddd70bb4465e62c24864b4c5f589
BornToLivee <bogdan.zinchenko.2019@gmail.com>
2025-03-21 17:58:46 +0200

    Implement novel search endpoint using VNDB API, returning structured NovelSearchResponse objects. Remove unused create_novel and create_tag endpoints for cleaner code.

--- vn-api-be/helper.py
+++ vn-api-be/helper.py
@@ -1,5 +1,7 @@
 import httpx
 
+from schemas import NovelSearchResponse
+
 VNDB_API_URL = "https://api.vndb.org/kana"
 
 async def search_vndb_novels_by_name(query: str):
@@ -12,5 +14,12 @@ async def search_vndb_novels_by_name(query: str):
         response = await client.post(f"{VNDB_API_URL}/vn", json=json_payload)
         
         if response.status_code == 200:
-            return response.json()["results"]
+            return [
+                NovelSearchResponse(
+                    id=novel["id"],
+                    title=novel["title"],
+                    image_url=novel["image"]["url"] if "image" in novel else ""
+                )
+                for novel in response.json()["results"]
+            ]
         return []
--- vn-api-be/main.py
+++ vn-api-be/main.py
@@ -5,6 +5,7 @@ from sqlalchemy.orm import Session
 
 from .models import Novel, SessionLocal, Tag
 from .schemas import NovelCreate, NovelResponse, TagCreate
+from .helper import search_vndb_novels_by_name
 
 app = FastAPI()
 
@@ -16,59 +17,14 @@ def get_db():
     finally:
         db.close()
 
+# TODO ensuure correct query
+@app.get("/novels/search", response_model=List[NovelResponse])
+async def novel_search(query: str):
+    novels = await search_vndb_novels_by_name(query)
+    return novels
+
 
 @app.get("/novels/", response_model=List[NovelResponse])
 def read_novels(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
     novels = db.query(Novel).offset(skip).limit(limit).all()
     return novels
-
-
-@app.post("/novels/", response_model=NovelCreate)
-def create_novel(novel: NovelCreate, db: Session = Depends(get_db)):
-    db_novel = Novel(
-        title=novel.title, 
-        description=novel.description,
-        my_rating=novel.my_rating
-        )
-    
-    db.add(db_novel)
-    db.commit()
-    db.refresh(db_novel)
-
-    if novel.tags_id:
-        for tag_id in novel.tags_id:
-            tag = db.query(Tag).filter(Tag.id == tag_id).first()
-            if tag:
-                db_novel.tags.append(tag)
-            else:
-                 raise HTTPException(status_code=404, detail=f"Tag with id {tag_id} not found")
-            
-        db.commit()
-        db.refresh(db_novel)
-
-    return db_novel
-
-
-@app.post("/tags/", response_model=TagCreate)
-def create_tag(tag: TagCreate, db: Session = Depends(get_db)):
-    db_tag = Tag(
-        name=tag.name,
-        description=tag.description
-    )
-
-    db.add(db_tag)
-    db.commit()
-    db.refresh(db_tag)
-
-    if tag.novels_id:
-        for novel_id in tag.novels_id:
-            novel = db.query(Novel).filter(Novel.id == novel_id).first()
-            if novel:
-                db_tag.novels.append(novel)
-            else:
-                raise HTTPException(status_code=404, detail=f"Novel with id {novel_id} not found")
-            
-        db.commit()
-        db.refresh(db_tag)
-    
-    return db_tag
