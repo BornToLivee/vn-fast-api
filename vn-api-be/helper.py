e496ba3861cd72a188864335e01579f5aa75494e
BornToLivee <bogdan.zinchenko.2019@gmail.com>
2025-03-21 23:50:13 +0200

    Add function to fetch VNDB novel details by ID, utilizing requests for API interaction. Update import statement for NovelSearchResponse.

--- vn-api-be/helper.py
+++ vn-api-be/helper.py
@@ -1,6 +1,7 @@
 import httpx
+import requests
 
-from schemas import NovelSearchResponse
+from .schemas import NovelSearchResponse
 
 VNDB_API_URL = "https://api.vndb.org/kana"
 
@@ -23,3 +24,22 @@ async def search_vndb_novels_by_name(query: str):
                 for novel in response.json()["results"]
             ]
         return []
+    
+
+def fetch_vndb_novel(novel_id: str):
+        json_payload = {
+        "filters": ["id", "=", novel_id],
+        "fields": "id, title, released, image.url, length, length_minutes, description, rating, votecount, developers.name"
+    }
+        
+        response = requests.post(f"{VNDB_API_URL}/vn", json=json_payload)
+            
+        if response.status_code != 200:
+            return None
+        
+        data = response.json()
+
+        if "results" not in data:
+            return None
+        
+        return data["results"][0]
