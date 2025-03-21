630382bbe3b098b879ea8c13c45aa062691a987c
BornToLivee <bogdan.zinchenko.2019@gmail.com>
2025-03-21 17:49:17 +0200

    Enhance Novel model and schemas with additional fields for detailed information, including review, status, language, and VNDB attributes.

--- vn-api-be/models.py
+++ vn-api-be/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Table, ForeignKey
+from sqlalchemy import Column, Integer, String, Table, ForeignKey, Text, Date, Float
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -15,11 +15,25 @@ novel_tag = Table(
 
 class Novel(Base):
     __tablename__ = "novels"
+    # My columns
     id = Column(Integer, primary_key=True)
-    title = Column(String, nullable=False, unique=True)
-    description = Column(String, nullable=True, unique=False)
-    my_rating = Column(Integer, nullable=True, unique=False)
     tags = relationship("Tag", secondary="novel_tag", back_populates="novels")
+    my_review = Column(Text, nullable=True)
+    status = Column(String, default="reading")
+    language = Column(String, default="russian")
+    my_rating = Column(Integer, nullable=True, unique=False)
+    # VNDB columns
+    vndb_id = Column(String, unique=True, nullable=False)
+    title = Column(String, nullable=False)
+    description = Column(Text, nullable=True)
+    image_url = Column(String)
+    studio = Column(String)
+    released = Column(Date)
+    length = Column(Integer) # from 1 to 5
+    length_minutes = Column(Integer, nullable=True)
+    rating = Column(Float, nullable=True)
+    votecount = Column(Integer)  # "reading" или "completed"
+    user_rating = Column(Integer, default=0)
 
 
 class Tag(Base):
--- vn-api-be/schemas.py
+++ vn-api-be/schemas.py
@@ -1,12 +1,33 @@
 from pydantic import BaseModel
 from typing import List, Optional
+from datetime import date
 
 
-class NovelResponse(BaseModel):
+class NovelsListResponse(BaseModel):
     id: int
     title: str
-    description: Optional[str]
+    status: str
     my_rating: int 
+    tags: List["TagNovelResponce"] = []
+
+
+class NovelsDetailResponse(BaseModel):
+    id: int
+    my_review: Optional[str] = None
+    status: str
+    language: str
+    my_rating: Optional[int] = None
+    vndb_id: str
+    title: str
+    description: Optional[str] = None
+    image_url: Optional[str] = None
+    studio: Optional[str] = None
+    released: Optional[date] = None
+    length: Optional[int] = None  # 1-5
+    length_minutes: Optional[int] = None
+    rating: Optional[float] = None
+    votecount: Optional[int] = None
+    user_rating: int
     tags: List["TagNovelResponce"] = []
 
 
@@ -16,11 +37,11 @@ class TagNovelResponce(BaseModel):
 
 
 class NovelCreate(BaseModel):
-    id: int
-    title: str
-    description: Optional[str]
-    my_rating: int 
-    tags_id: Optional[List[int]] = []
+    search_name: str  # ID новеллы на VNDB
+    status: str = "reading"  # Статус: "reading", "completed" или другие
+    my_review: Optional[str] = None  # Личный обзор
+    my_rating: Optional[int] = None  # Личная оценка
+    tags: List[str] = []
 
 
 class TagCreate(BaseModel):
