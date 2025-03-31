834e8f249bfb88141b84878d467a86c1351dec92
BornToLivee <bogdan.zinchenko.2019@gmail.com>
2025-03-31 14:14:29 +0300

    Initialize Alembic migration setup with configuration files and initial migration scripts. Remove empty __init__.py file and add database connection handling in settings.py.

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,2 +1,1 @@
 VNDB_API_URL = "https://api.vndb.org/kana"
-DATABASE_URL = "sqlite:///D:/PythonProjects/Portfolios/vn-fast-api/backend/app/database/testnovels.db"
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,19 @@
 
 import sys
 import os
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from fastapi import FastAPI
 from app.api.novels import router as novels_router
+from app.database.settings import get_db
 
 app = FastAPI()
 
+
+@app.get("/test-db")
+def test_db(db: Session = Depends(get_db)):
+    return {"message": "Database connected!"}
 app.include_router(novels_router)
