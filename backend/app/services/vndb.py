249e12d2cf83a06ee0d58190e11a7ea01dde106f
BornToLivee <bogdan.zinchenko.2019@gmail.com>
2025-04-10 13:26:36 +0300

    Add new tag filtering functionality in VNDB service
    - Introduced constants for acceptable tag importance rating and spoiler value in config.py.
    - Enhanced vndb.py to include a new asynchronous function for fetching and filtering novel tags based on specified criteria.
    - Updated import statements for better organization and clarity.

--- backend/app/services/vndb.py
+++ backend/app/services/vndb.py
@@ -3,7 +3,11 @@ import requests
 
 from app.schemas.novel import NovelSearchResponse
 from app.core.logger import logger
-from app.core.config import VNDB_API_URL
+from app.core.config import (
+    ACCEPTABLE_TAGS_IMPORTANCE_RATING as ATIR,
+    ACCEPTABLE_TAGS_SPOILER_VALUE as ATSV,
+    VNDB_API_URL,
+)
 
 
 async def search_vndb_novels_by_name(query: str):
@@ -30,7 +34,7 @@ async def search_vndb_novels_by_name(query: str):
         else:
             logger.log("ERROR", f"Error searching for novels: {response.status_code} - {response.text}")
             return []
-    
+        
 
 def fetch_vndb_novel(novel_id: str):
     json_payload = {
@@ -54,3 +58,28 @@ def fetch_vndb_novel(novel_id: str):
     
     logger.log("INFO", f"Successfully fetched novel: {data['results'][0]['title']} with ID: {novel_id}")
     return data["results"][0]
+
+
+async def fetch_vndb_all_novel_tags(novel_id: str):
+    filtered_tags = []
+    json_payload = {
+        "filters": ["id", "=", novel_id],
+        "fields": "tags.rating, tags.name, tags.category, tags.spoiler"
+    }
+
+    async with httpx.AsyncClient() as client:
+        try:
+            response = await client.post(f"{VNDB_API_URL}/vn", json=json_payload)
+            if response.status_code == 200:
+                all_tags = response.json()["results"]
+                for tags in all_tags:
+                    for tag in tags:
+                        if tag["category"] != "ero" and tag["rating"] > ATIR and tag["spoiler"] <= ATSV:
+                            filtered_tags.append(tag["name"])
+            else:
+                logger.log("WARNING", f"Api error, impossible fetch tags for novel {novel_id}")
+                return []
+        except Exception as e:
+            logger.log("WARNING", f"Unexpected error {e}, impossible fetch tags for novel {novel_id}")
+
+    return filtered_tags
